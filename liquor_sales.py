0916ce46fdce972f46defd7e0d5d1b280cf6ecea
fdiamant <fotis.diamantopoulos@gmail.com>
2023-04-11 18:29:22 +0300

    Calculations

--- liquor_sales.py
+++ liquor_sales.py
@@ -17,10 +17,21 @@ missing = df[df.isna().any(axis=1)]
 # calculate percentage of missing values per column
 missing_percentages = round((df.isna().mean() * 100), 2)
 # Print the results
-print('Rows with missing or null values:\n', missing)
-print('Percentage of missing values per column:\n', missing_percentages)
+# print('Rows with missing or null values:\n', missing)
+# print('Percentage of missing values per column:\n', missing_percentages)
 # After calculating the percentage of missing values per column, it becomes evident that a significant portion of the
 # missing value data is observed in the store_location column (12.16%) and the category_name column (8.11%). However,
 # as the significant columns here are not category_name and store_location, it is not necessary to drop the indexes
 # with missing values.
+
+popular_items = df.groupby(['zip_code','item_description'])['bottles_sold'].sum()
+popular_items = popular_items.sort_index(level=['zip_code', 'item_description'])
+
+sales_per_store = df.groupby(['store_number', 'store_name'])['sale_dollars'].sum()
+total_sales = df['sale_dollars'].sum()
+sales_percentage = round(100*sales_per_store/total_sales,2)
+sales_percentage = sales_percentage.sort_values(ascending=False)
+
+print('The most popular items per zip code are:\n', popular_items)
+print('The sales per store are:\n', sales_percentage)
 
